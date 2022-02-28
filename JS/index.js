d61a5d5aa3700107697265f9c2faa0729fb44245
Felix Stark <felix.b.stark@gmail.com>
2022-02-28 12:16:09 +0100

    start

---
+++ JS/index.js
@@ -0,0 +1,16 @@
+/**  
+ * endpoints: 
+ * get key - '/keys' - returnerar API nyckel.
+ * get bodies - '/bodies' - returnerar alla stora himlakroppar i vårt solsystem.
+ * dela upp bodies till singel object
+ * display vald body i UI
+ * klicka på body för att få fram information
+ * klicka för att komma tillbaka till mainpage
+*/
+
+import { getAPIKey, getBodies } from "./MODULES/API";
+
+
+
+getAPIKey()
+console.log(bodies)
